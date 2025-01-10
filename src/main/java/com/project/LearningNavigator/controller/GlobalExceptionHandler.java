84c539fb8b63f93b4fc614ac3acbb61cf34c9127
Anshu Geetanjali <ageetanjali308@gmail.com>
2025-01-10 22:43:59 +0530

    Update GlobalExceptionHandler and exception package

--- src/main/java/com/project/LearningNavigator/controller/GlobalExceptionHandler.java
+++ src/main/java/com/project/LearningNavigator/controller/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.project.LearningNavigator.exception.DatabaseOperationException;
+import com.project.LearningNavigator.exception.InvalidOperationException;
 import com.project.LearningNavigator.exception.ResourceNotFoundException;
 
 @RestControllerAdvice
@@ -21,9 +22,14 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<> ("DataBase error: "+ ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(InvalidOperationException.class)
+    public ResponseEntity<?> handleInvalidOperationException(InvalidOperationException ex) {
+        return new ResponseEntity<> ("Invalid operation: "+ ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handlegenericException(Exception ex) {
         return new ResponseEntity<> ("An unexpected error occurred: "+ ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
 
 }
---
+++ src/main/java/com/project/LearningNavigator/exception/InvalidOperationException.java
@@ -0,0 +1,8 @@
+package com.project.LearningNavigator.exception;
+
+public class InvalidOperationException extends RuntimeException{
+    
+    public InvalidOperationException(String message){
+        super(message);
+    }
+}
